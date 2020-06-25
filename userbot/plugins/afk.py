16a71e0c25bbbd62c25655d56a791707b8b49e95
saskeuday <67370201+saskeuday@users.noreply.github.com>
2020-06-25 18:28:12 +0300

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -146,11 +146,11 @@ async def on_afk(event):
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = f"__سيدي ѕᴀѕᴋᴇ ʟ̤ɾʅ丂ɹɹɹȊɹɹɹ قد ذهب :full_moon_with_face::herb: منذ__ `{total_afk_time}`\nاين هو الان :expressionless:؟: ~~وحده الله يعلم :full_moon_with_face::herb:~~ " + \
-            f"\n\n__أعدك بأنني سأعود بعد بضع سنوات ضوئية ☃️:fire:__\n**REASON**: {reason}" \
+            f"\n\n__أعدك بأنني سأعود بعد بضع سنوات ضوئية ☃️:fire:__\n**رساله من سيدي**: {reason}" \
             if reason \
             else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
         msg = await event.reply(message_to_reply)
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
             await last_afk_message[event.chat_id].delete()  # pylint:disable=E0602
         last_afk_message[event.chat_id] = msg  # pylint:disable=E0602
